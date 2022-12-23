93559c75d67825519736092534d9cdaa119209f8
Vijay Upadhyay <vijay.upadhyay92@gmail.com>
2022-12-23 16:25:18 +0530

    test bin files

--- DataStructure/src/com/vijay/array/MajorityElementInArray.java
+++ DataStructure/src/com/vijay/array/MajorityElementInArray.java
@@ -3,38 +3,36 @@ package com.vijay.array;
 public class MajorityElementInArray {
 
 	public static void main(String[] args) {
-		
-		int[] array = {2,3,2,1,2,3,2,2,2,2,6,5,9,2};
-		
+
+		int[] array = { 2, 3, 2, 1, 2, 3, 2, 2, 2, 2, 6, 5, 9, 2 };
+
 		findMajorityElement(array);
 
 	}
-	
-	public static void findMajorityElement(int[] arr){
-		int n = arr.length,count=1,maxElementPos=0;
-		//int[] majorityElementPosition = new int[arr.length];
-		
-		for(int i=1;i<arr.length;i++){
-			
-			if(arr[maxElementPos] == arr[i])
+
+	public static void findMajorityElement(int[] arr) {
+		int n = arr.length, count = 1, maxElementPos = 0;
+		// int[] majorityElementPosition = new int[arr.length];
+
+		for (int i = 1; i < arr.length; i++) {
+
+			if (arr[maxElementPos] == arr[i])
 				count++;
 			else
 				count--;
-			
-			if(count == 0){
+
+			if (count == 0) {
 				maxElementPos = i;
-				count=1;
+				count = 1;
 			}
-			if(count > n/2){
+			if (count > n / 2) {
 				break;
-			}	
-			
-			
-			
+			}
+
 		}
-		
-		System.out.println("Majority element is "+arr[maxElementPos]);
-		
+
+		System.out.println("Majority element is " + arr[maxElementPos]);
+
 	}
 
 }
--- DataStructure/src/com/vijay/array/MajorityElementUsingHashMap.java
+++ DataStructure/src/com/vijay/array/MajorityElementUsingHashMap.java
@@ -5,24 +5,23 @@ import java.util.HashMap;
 public class MajorityElementUsingHashMap {
 
 	public static void main(String[] args) {
-		
-		int[] array = {2,3,2,1,1,2,3,4,2,1,2};
-		HashMap<Integer, Integer> map =new HashMap<Integer, Integer>();
-		int value=0;
-		
-		for(int i=0;i<array.length;i++){
-			
-			if(map.containsKey(array[i])){
+
+		int[] array = { 2, 3, 2, 1, 1, 2, 3, 4, 2, 1, 2 };
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int value = 0;
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (map.containsKey(array[i])) {
 				value = map.get(array[i]);
 				value++;
 				map.put(array[i], value);
-				value=0;
-	}
-			else
-					map.put(array[i], 1);
-			
-		}		
-		
+				value = 0;
+			} else
+				map.put(array[i], 1);
+
+		}
+
 	}
 
 }
