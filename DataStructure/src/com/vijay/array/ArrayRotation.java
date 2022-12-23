284c0363ed891f753cbe34b82621cefc63904665
Vijay Upadhyay <vijay.upadhyay92@gmail.com>
2022-12-23 16:07:42 +0530

    eclipse git setting

--- DataStructure/src/com/vijay/array/ArrayRotation.java
+++ DataStructure/src/com/vijay/array/ArrayRotation.java
@@ -2,24 +2,21 @@ package com.vijay.array;
 
 public class ArrayRotation {
 
-	public static void main(String[] args) {
-		int[] array = {22,3,44,56,75,89,12,322};
-		int rotateBy= 2;
-		int j = array.length-1;
-		for(int i=0;i<rotateBy;i++){
-			rotateOneByOne(array,i,j);
-			j--;
-		}
-			
+    public static void main(String[] args) {
+	int[] array = { 22, 3, 44, 56, 75, 89, 12, 322 };
+	int rotateBy = 2;
+	int j = array.length - 1;
+	for (int i = 0; i < rotateBy; i++) {
+	    rotateOneByOne(array, i, j);
+	    j--;
+	}
 
-	}
-	
-	public static void rotateOneByOne(int[] arr, int i,int j){
-	
-		int temp = arr[i];
-		
-		
-		
-	}
+    }
+
+    public static void rotateOneByOne(int[] arr, int i, int j) {
+
+	// int temp = arr[i];
+
+    }
 
 }
