b6531f87483e6f30fe61853188faa1c7e7b0d789
Vijay Upadhyay <vijay.upadhyay92@gmail.com>
2023-01-13 01:07:48 +0530

    tree ds

--- DataStructure/src/com/vijay/tree/BinarySearchTree.java
+++ DataStructure/src/com/vijay/tree/BinarySearchTree.java
@@ -2,68 +2,67 @@ package com.vijay.tree;
 
 public class BinarySearchTree {
 
-	class Node {
-		int key;
-		Node right, left;
+    class Node {
+	int key;
+	Node right, left;
 
-		Node(int key) {
-			this.key = key;
-			right = left = null;
-		}
-
-		@Override
-		public String toString() {
-			return "Node [key=" + key + ", right=" + right + ", left=" + left + "]";
-		}
+	Node(int key) {
+	    this.key = key;
+	    right = left = null;
 	}
 
-	Node root;
-
-	public BinarySearchTree() {
-		root = null;
+	@Override
+	public String toString() {
+	    return "Node [key=" + key + ", right=" + right + ", left=" + left + "]";
 	}
+    }
 
-	void insert(int key) {
-		root = insertRec(root, key);
+    Node root;
+
+    public BinarySearchTree() {
+	root = null;
+    }
+
+    void insert(int key) {
+	root = insertRec(root, key);
+    }
+
+    Node insertRec(Node root, int key) {
+	if (root == null) {
+	    root = new Node(key);
+	    return root;
 	}
+	if (key < root.key)
+	    root.left = insertRec(root.left, key);
+	else if (key > root.key)
+	    root.right = insertRec(root.right, key);
 
-	Node insertRec(Node root, int key) {
+	return root;
+    }
 
-		if (root == null) {
-			root = new Node(key);
-			return root;
-		}
-		if (key < root.key)
-			root.left = insertRec(root.left, key);
-		else if (key > root.key)
-			root.right = insertRec(root.right, key);
+    public void printTreeInorder() {
+	printTree(root);
+    }
 
-		return root;
+    void printTree(Node root) {
+	if (root != null) {
+	    printTree(root.left);
+	    System.out.println(root.key);
+	    printTree(root.right);
 	}
+    }
 
-	public void printTreeInoredr() {
-		printTree(root);
-	}
+    public static void main(String[] args) {
+	BinarySearchTree bst = new BinarySearchTree();
+	bst.insert(22);
+	bst.insert(24);
+	bst.insert(25);
+	bst.insert(2);
+	bst.insert(7);
+	bst.insert(1);
 
-	void printTree(Node root) {
-		if (root != null) {
-			printTree(root.left);
-			System.out.println(root.key);
-			printTree(root.right);
-		}
-	}
-
-	public static void main(String[] args) {
-		BinarySearchTree bst = new BinarySearchTree();
-		bst.insert(22);
-		bst.insert(24);
-		bst.insert(25);
-		bst.insert(2);
-		bst.insert(7);
-		bst.insert(1);
-
-		// bst.printTree(bst.root);
-		bst.printTreeInoredr();
-	}
+	// bst.printTree(bst.root);
+	bst.printTreeInorder();
+    }
 
 }
