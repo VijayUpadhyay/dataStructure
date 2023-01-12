414e60efc6dadb68bee84d51d4a1d79aebb2f8fd
Vijay Upadhyay <vijay.upadhyay92@gmail.com>
2023-01-13 01:06:22 +0530

    string ds

--- DataStructure/src/com/vijay/string/StringFeatureTest.java
+++ DataStructure/src/com/vijay/string/StringFeatureTest.java
@@ -2,10 +2,9 @@ package com.vijay.string;
 
 public class StringFeatureTest {
 
-	public static void main(String[] args) {
-		String s = "abc df";
-		String b  = s.substring(1);
-		System.out.println(b);
-
-	}
+    public static void main(String[] args) {
+	String s = "abc df";
+	String b = s.substring(1);
+	System.out.println(b);
+    }
 }
